cdd4aadcf0e9f5bbc5b6e9d17ec1fd927d70a044
ClassicOldSong <nutosservice@gmail.com>
2022-09-22 07:50:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# singui-native-experiment
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/singui-native-experiment)
---
+++ app/app.js
@@ -0,0 +1,86 @@
+import * as NSCore from '@nativescript/core';
+// eslint-disable-next-line no-duplicate-imports
+import { Application } from '@nativescript/core';
+import { isAndroid, isIOS } from '@nativescript/core/platform';
+import { alert } from '@nativescript/core/ui/dialogs';
+import { document } from 'dominative';
+import {
+  browser,
+  text,
+  attr,
+  prop,
+  setGlobalCtx,
+  useTags,
+  useElement,
+  build,
+  on,
+} from 'singui';
+
+global.NSCore = NSCore;
+global.document = document;
+
+setGlobalCtx(browser());
+
+const tags = useTags(false);
+
+const app = () =>
+  build(() => {
+    const {
+      Frame,
+      Page,
+      ActionBar,
+      NavigationButton,
+      ActionItem,
+      StackLayout,
+      Label,
+      Button,
+    } = tags;
+
+    let frameElement = null;
+
+    Frame(() => {
+      frameElement = useElement();
+      Page(() => {
+        ActionBar(() => {
+          prop.title = 'Hello World!';
+          NavigationButton(() => {
+            prop.text = 'Go Back';
+            if (isAndroid) prop.android.systemIcon = 'ic_menu_back';
+          });
+          ActionItem(() => {
+            prop.text = 'Alert';
+            if (isIOS) prop.ios.position = 'right';
+            on('tap', () => {
+              alert('This is an alert!');
+            });
+          });
+        });
+
+        StackLayout(() => {
+          Label(() => {
+            prop.className = 'center';
+            text('Welcome to SingUI!');
+          });
+          Button(() => {
+            attr.class = '-primary';
+            let count = 0;
+            const setText = text().$textContent(
+              (val) => `You have clicked ${count} times!`
+            );
+            on('tap', () => {
+              count += 1;
+              setText(count);
+            });
+
+            setText();
+          });
+        });
+      });
+    });
+
+    return frameElement;
+  });
+
+Application.run({
+  create: () => app(),
+});
